647125375884f255ef5ba430f892fc6afa5a780b
joaquinavalos20 <joayascri@gmail.com>
2024-11-26 03:00:35 -0300

    ARREGLO TAB

--- tarapay/TARAPAY/navigation/tabs.js
+++ tarapay/TARAPAY/navigation/tabs.js
@@ -1,215 +1,92 @@
 import React from "react";
-import {
-    View,
-    Image,
-    TouchableOpacity,
-    StyleSheet
-} from "react-native";
-import { createBottomTabNavigator, BottomTabBar } from "@react-navigation/bottom-tabs"
-import Svg, {
-    Path
-} from 'react-native-svg'
-import { isIphoneX } from 'react-native-iphone-x-helper'
+import { Image, StyleSheet } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import { Home, Scan } from "../screens"
-import { COLORS, icons } from "../constants"
+import Home from "../screens/Home"; // Asegúrate de que esta ruta sea correcta
+import Scan from "../screens/Scan";
 import Profile from "../screens/Profile";
+import { COLORS, icons } from "../constants"; // Asegúrate de tener un archivo constants.js con estos valores
 
-const Tab = createBottomTabNavigator()
-
-const TabBarCustomButton = ({ accessibilityLabel, accessibilityState, children, onPress }) => {
-
-    var isSelected = accessibilityState.selected
-
-    if (isSelected) {
-        return (
-            <View style={{ flex: 1, alignItems: 'center' }}>
-                <View
-                    style={{
-                        flexDirection: 'row',
-                        position: 'absolute',
-                        top: 0
-                    }}
-                >
-                    <View style={{ flex: 1, backgroundColor: COLORS.white }}></View>
-                    <Svg
-                        width={75}
-                        height={61}
-                        viewBox="0 0 75 61"
-                    >
-                        <Path
-                            d="M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
-                            fill={COLORS.white}
-                        />
-                    </Svg>
-                    <View style={{ flex: 1, backgroundColor: COLORS.white }}></View>
-                </View>
-
-                <TouchableOpacity
-                    style={{
-                        top: -22.5,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        width: 50,
-                        height: 50,
-                        borderRadius: 25,
-                        backgroundColor: COLORS.primary,
-                        ...styles.shadow
-                    }}
-                    onPress={onPress}
-                >
-                    {children}
-                </TouchableOpacity>
-            </View>
-        )
-    } else {
-        return (
-            <TouchableOpacity
-                style={{
-                    flex: 1,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    width: 50,
-                    height: 50,
-                    backgroundColor: COLORS.white
-                }}
-                activeOpacity={1}
-                onPress={onPress}
-            >
-                {children}
-            </TouchableOpacity>
-        )
-    }
-}
-
-const CustomTabBar = (props) => {
-    if (isIphoneX()) {
-        return (
-            <View>
-                <View
-                    style={{
-                        position: 'absolute',
-                        bottom: 0,
-                        left: 0,
-                        right: 0,
-                        height: 30,
-                        backgroundColor: COLORS.white
-                    }}
-                ></View>
-                <BottomTabBar {...props.props} />
-            </View>
-        )
-    } else {
-        return (
-            <BottomTabBar {...props.props} />
-        )
-    }
-}
+const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
-    return (
-        <Tab.Navigator
-            screenOptions={{
-                headerShown: false,
-                tabBarShowLabel: false,
-                tabBarStyle: {
-                    position: 'absolute',
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    elevation: 0,
-                    backgroundColor: "transparent",
-                    borderTopColor: "transparent",
-                }
-            }}
-            tabBar={(props) => (
-                <CustomTabBar
-                    props={props}
-                />
-            )}
-        >
-            <Tab.Screen
-                name="Home"
-                component={Home}
-                options={{
-                    tabBarIcon: ({ focused }) => (
-                        <Image
-                            source={icons.more}
-                            resizeMode="contain"
-                            style={{
-                                width: 25,
-                                height: 25,
-                                tintColor: focused ? COLORS.white : COLORS.secondary
-                            }}
-                        />
-                    ),
-                    tabBarButton: (props) => (
-                        <TabBarCustomButton
-                            {...props}
-                        />
-                    )
-                }}
-            />
-            <Tab.Screen
-                name="Scan"
-                component={Scan}
-                options={{
-                    tabBarIcon: ({ focused }) => (
-                        <Image
-                            source={icons.scan}
-                            resizeMode="contain"
-                            style={{
-                                width: 25,
-                                height: 25,
-                                tintColor: focused ? COLORS.white : COLORS.secondary
-                            }}
-                        />
-                    ),
-                    tabBarButton: (props) => (
-                        <TabBarCustomButton
-                            {...props}
-                        />
-                    )
-                }}
-            />
-            <Tab.Screen
-                name="Profile"
-                component={Profile}
-                options={{
-                    tabBarIcon: ({ focused }) => (
-                        <Image
-                            source={icons.user}
-                            resizeMode="contain"
-                            style={{
-                                width: 25,
-                                height: 25,
-                                tintColor: focused ? COLORS.white : COLORS.secondary
-                            }}
-                        />
-                    ),
-                    tabBarButton: (props) => (
-                        <TabBarCustomButton
-                        {...props}
-                        />
-                    )
-                }}
-            />
-                   </Tab.Navigator>
-    )
-}
-
-const styles = StyleSheet.create({
-    shadow: {
-        shadowColor: COLORS.primary,
-        shadowOffset: {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          position: "absolute",
+          bottom: 10, // Subir la barra ligeramente
+          left: 10,
+          right: 10,
+          height: 70, // Altura de la barra
+          backgroundColor: COLORS.white,
+          borderRadius: 15, // Redondear las esquinas
+          shadowColor: "#000",
+          shadowOffset: {
             width: 0,
-            height: 10,
+            height: 4,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 4,
+          elevation: 5,
         },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-
-        elevation: 5
-    }
-})
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={icons.more} // Cambia este ícono si es necesario
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? COLORS.primary : COLORS.secondary,
+              }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Scan"
+        component={Scan}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={icons.scan} // Cambia este ícono si es necesario
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? COLORS.primary : COLORS.secondary,
+              }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={icons.user} // Cambia este ícono si es necesario
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? COLORS.primary : COLORS.secondary,
+              }}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default Tabs;
